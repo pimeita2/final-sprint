a52cb826ac25f4c1b1371bbb0e040bf31ec16782
reut <reut1990@gmail.com>
2018-11-26 18:27:27 +0200

    design template1

---
+++ frontend/src/views/template1.vue
@@ -0,0 +1,24 @@
+<template>
+     <section class="template1">
+        <div class="col-1">
+        </div>
+        <div class="col-2">
+
+        </div>
+     </section>
+</template>
+
+<style lang="scss">
+  .template1{
+      display: grid;
+       grid-template-columns: 50% 50%;
+  }
+
+  .col-1{
+      background-color: green;
+  }
+  .col-2{
+      background-color:black;
+        }
+
+</style>
