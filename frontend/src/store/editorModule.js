4daf7225605db4a5fc962fa68f90289eb5060a40
rotemkaslsi <ksl.rotem@gmail.com>
2018-12-01 11:42:27 +0200

    first and editor change

--- frontend/src/store/editorModule.js
+++ frontend/src/store/editorModule.js
@@ -1,5 +1,10 @@
 export default {
     state: {
+        // userStyle: {
+        //     background: {
+        //         backgroundColor: '#ff9a90',
+        //         backgroundImage: ``,
+            
         userStyle: [
             {
                 cmpPartName: 'invaitorName',
@@ -21,46 +26,51 @@ export default {
 
             },
             {
-                backgroundColor: {
-                    backgroundColor: '#ff9a90'
+                background: {
+                    backgroundColor: '#ff9a90',
+                    backgroundImage: ``,
                 }
             }
 
-        ]
-    },
+        ]},
+    // },
 
 
 
     mutations: {
         setBackground(state, { field, css }) {
-            state.userStyle[field] = css;
+            // console.log('in set Background', state.userStyle[state.userStyle.length-1], css);
+            state.userStyle[state.userStyle.length-1].background = css;
         },
         setUserStyle(state, { field, css, currCmpPart }) {
-            console.log('set user style has', field, css, currCmpPart, 'and in state', state.userStyle);
+            // console.log('set user style has', field, css, currCmpPart, 'and in state', state.userStyle);
             state.userStyle.map(obj => {
                 if (obj.cmpPartName === currCmpPart) obj.style[field] = css[field];
-                console.log('in state', state.userStyle);
+                // console.log('in state', state.userStyle);
             })
-            console.log('new user style pref:', state.userStyle);
+            // console.log('new user style pref:', state.userStyle);
         }
     },
 
     actions: {
         setBackgroundStyle(context, { field, css }) {
-            context.commit({ type: 'setUserStyle', field, css })
+            // console.log('in setBackgroundStyle', field, css);
+            context.commit({ type: 'setBackground', field, css })
         },
         setUserStyleOfCmp(context, { field, css, currCmpPart }) {
-            console.log(field, 'field')
+            // console.log(field, 'field')
             context.commit({ type: 'setUserStyle', field, css, currCmpPart })
         }
     },
 
     getters: {
-        // userStyleBackground(state) {
-        //     return state.userStyle.backgroundColor;
-        // },
+        userStyleBackground(state) {
+            return state.userStyle[state.userStyle.length-1].background;
+        },
         getUserStyle(state) {
             return state.userStyle;
         },
     }
 }
+// }
+// }
