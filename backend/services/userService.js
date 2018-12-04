d191f799d8a1218abbcfbe9c8e1cdbb38d641697
rotemkaslsi <ksl.rotem@gmail.com>
2018-12-04 08:50:00 +0200

    fix

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -5,37 +5,79 @@ const ObjectId = require('mongodb').ObjectId;
 module.exports = {
     query,
     getById,
-
+    remove,
+    add,
+    update
 
 }
-
-
 function query() {
     return mongoService.connectToDb()
         .then(dbConn => {
-            const templateCollection = dbConn.collection('user');
-            return templateCollection.find().toArray();
+            const collection = dbConn.collection('user');
+            return collection.find().toArray();
+        })
+        .catch(err => {
+            console.log('err:', err);
         })
 }
 
-function getById(id) {
-    const _id = new ObjectId(id)
+
+function getById(userId) {
+    const userId = new ObjectId(userId)
     return mongoService.connectToDb()
-        .then(db => db.collection('user').findOne({ _id }))
+        .then(dbConn => {
+            const userCollection =dbConn.collectiom('user');
+            return userCollection.findOne({_id: userId})
+            })
 }
 
-
-
-function addUser({ nickname }) {
-    var user = { nickname }
-    return mongoService.connect()
-        .then(db => db.collection('user').insertOne(user))
-        .then(res => {
-            user._id = res.insertedId
-            return user
+function remove(userId) {
+    userId = new ObjectId(userId)
+    return mongoService.connectToDb()
+        .then(dbConn => {
+            const userCollection = dbConn.collection('user');
+            return userCollection.remove({ _id: userId })
         })
 }
+
+
+function add(user) {
+    user.modified = new Date(user.modified)
+    return mongoService.connectToDb()
+        .then(dbConn => {
+            const collectiom = dbConn.collection('user');
+            return collectiom.insertOne(user)
+                .then(result => {
+                    user._id = result.insertedId;
+                    return user;
+                })
+        })
+}
+
+function update(user) {
+    const userId = new ObjectId(user.id)
+    return mongoService.connectToDb()
+        .then(dbConn => {
+            const userCollection = dbConn.collection('user');
+            return userCollection.updateOne({ _id: userId }, { $set: user })
+                .then(result => {
+                    return user;
+                });
+
+        })
+}
+
 function checkLogin({ nickname }) {
     return mongoService.connect()
         .then(db => db.collection('user').findOne({ nickname }))
 }
+
+
+
+
+
+
+
+
+
+
